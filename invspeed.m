f8b90892eed2c39b3f17aa35e7c03d4ae17b816a
Long Dang <117198788+micho0802@users.noreply.github.com>
2024-08-02 15:59:47 -0500

    Delete invspeed.m

--- invspeed.m
+++
@@ -1,9 +0,0 @@
-function lot(3, 6000)
-%lot picks lottery numbers. N numbers from 1 to R are chosen.
-if (3 <= 600 & 600 > 0)
-    tot = randperm(600);
-%  returns  a  row  vector  containing  a  random  permutation  of  the  integers  from  1  to n without 
-repeating %elements.
-    x = tot(1:3);
-    disp(['Your lottery numbers are: ', num2str(x)])
-end
